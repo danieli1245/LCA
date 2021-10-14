c7a7099b4e68189bcea65afb699fd9406cea9bd2
danieli1245 <ilyind@tcd.ie>
2021-10-14 21:08:39 +0100

    Initial commit

---
+++ src/LCA.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LCA {
+    Node root;
+    public List<Integer> path1 = new ArrayList<>();
+    public List<Integer> path2 = new ArrayList<>();
+
+
+    int findLCA(int n1, int n2) {
+
+        path1.clear();
+        path2.clear();
+        return findLCAInt(root, n1, n2);
+    }
+
+    private int findLCAInt(Node root, int n1, int n2) {
+        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) return -1;
+
+        int i;
+        for (i = 0; i < path1.size() && i < path2.size(); i++) {
+            if (!path1.get(i).equals(path2.get(i)))
+                break;
+        }
+
+        return path1.get(i - 1);
+    }
+
+    public boolean findPath(Node root, int n, List<Integer> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root.data);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        if (root.left != null && findPath(root.left, n, path)) {
+            return true;
+        }
+
+        if (root.right != null && findPath(root.right, n, path)) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+
+        return false;
+    }
+
+}
